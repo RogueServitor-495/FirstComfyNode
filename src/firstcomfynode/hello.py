b4bc5d8c6eb7bbbb04ffc355d979d00a946677ed
chenpenghui <chenph@bupt.edu.cn>
2025-02-18 18:08:39 +0800

    edited .gitignore

--- src/firstcomfynode/hello.py
+++ src/firstcomfynode/hello.py
@@ -1,3 +1,5 @@
+import torch
 
 if __name__ == "__main__":
+    print(torch.cuda.is_available())
     print("Hello, World!")
--- src/firstcomfynode/nodes.py
+++ src/firstcomfynode/nodes.py
@@ -1,4 +1,7 @@
 from inspect import cleandoc
+import torch
+from torch import Tensor
+from typing import List
 class Example:
     """
     A example node
@@ -116,3 +119,19 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Example": "Example Node"
 }
+
+class ImageSelector:
+    CATEGORY = "example"
+    @classmethod    
+    def INPUT_TYPES(s):
+        return { "required":  { "images": ("IMAGE",), } }
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "choose_image"
+
+# select the brightest image among an image list
+def choose_image(self, images:List[Tensor]):
+    brightness = list(torch.mean(image.flatten().item() for image in images))
+    brightest = brightness.index(max(brightness))
+    result = images[brightest].unsqueeze(0)
+    return (result,)
+       
